eea97c1686eecb72dbc17d28d52c729d4f073a63
kevinstring <kevingabrielmdn@gmail.com>
2024-01-02 16:47:21 -0600

    cambioslap2

--- src/app/modal-registrar-venta/modal-registrar-venta.component.ts
+++ src/app/modal-registrar-venta/modal-registrar-venta.component.ts
@@ -5,13 +5,17 @@ import { IonicModule } from '@ionic/angular';
 import { ServicioService } from '../servicio.service';
 import { ChangeDetectorRef } from '@angular/core';
 import { NgZone } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-modal-registrar-venta',
   templateUrl: './modal-registrar-venta.component.html',
   styleUrls: ['./modal-registrar-venta.component.scss'],
   standalone:true,
-  imports:[IonicModule,CommonModule,ReactiveFormsModule,FormsModule]
+  imports:[IonicModule,CommonModule,ReactiveFormsModule,FormsModule],
+  providers: [MessageService],
+
 })
 export class ModalRegistrarVentaComponent  implements OnInit {
   ventaRegistrada=false
@@ -23,7 +27,7 @@ export class ModalRegistrarVentaComponent  implements OnInit {
   productoVenta:any={}
   arregloProductos:any=[]
   indice:any
-  constructor(private servicio:ServicioService,private cdRef: ChangeDetectorRef,private zone: NgZone) { }
+  constructor(private servicio:ServicioService,private cdRef: ChangeDetectorRef,private zone: NgZone,private mensaje:MessageService) { }
 
   ngOnInit() {
     this.getCategorias()
@@ -47,6 +51,8 @@ export class ModalRegistrarVentaComponent  implements OnInit {
       next:(data:any)=>{
         this.loading=false
         console.log(data)
+        this.ventaRealizada=data.venta;
+
         this.ventaRegistrada=true
       },
       error:(err:any)=>{
@@ -87,7 +93,7 @@ consultaProductos(idcategoria:any){
     next:(data:any)=>{
       this.productos=data.productos
       console.log(data)
-      this.ventaRealizada=data.venta;
+      console.log(this.ventaRealizada)
     },  
     error:(err:any)=>{
       console.log(err)
@@ -98,20 +104,34 @@ consultaProductos(idcategoria:any){
 agregarProducto(producto: any,i:any) {
   this.indice=i;
   const index = this.arregloProductos.findIndex(p => p.ID_PRODUCTO === producto.ID_PRODUCTO);
-
+const form = new FormData();
+form.append('codigoProducto',producto.CODIGO)
+form.append('idVenta',this.ventaRealizada)
   if (index !== -1) {
     // El producto ya está en el carrito, incrementar la cantidad
     this.arregloProductos[index].cantidadSeleccionada += 1;
+    form.append('cantidad',this.arregloProductos[index].cantidadSeleccionada)
+
   } else {
     // El producto no está en el carrito, agregarlo con cantidad 1
     this.arregloProductos.push({ ...producto, cantidadSeleccionada: 1 });
+    form.append('cantidad','1');
   }
   console.log(this.arregloProductos)
-let form = new FormData()
-form.append('codigoProducto',producto.ID_PRODUCTO)
-form.append('idVenta',this.ventaRealizada)
-form.append('cantidad',this.arregloProductos)
-  // this.servicio.post('postComanda',)
+
+
+  this.servicio.post('postComanda',form).subscribe({
+    next:(data:any)=>{
+      console.log(data)
+      this.mensaje.add({severity:'success',summary:'Exito',detail:'Se ha agregado el producto'})
+
+
+    },  
+    error:(err:any)=>{
+      console.log(err)
+      this.mensaje.add({severity:'error',summary:'Error',detail:'Ha ocurrido un error al agregar el producto'})
+    }
+  })
 
 
   // let form = new FormData()
